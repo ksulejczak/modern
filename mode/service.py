dad6d9179c7e1455b1b5bd5e0c5e1a0607aa32c7
Krzysztof Sulejczak <krzysiek@sulejczak.eu>
2021-12-15 13:58:29 +0100

    Add start, stop, from_awaitable methods

--- mode/service.py
+++ mode/service.py
@@ -1,7 +1,32 @@
+from __future__ import annotations
+
+import asyncio
+import enum
 from contextlib import AbstractAsyncContextManager, AbstractContextManager
-from typing import Any
+from functools import wraps
+from typing import Any, Awaitable, Callable, Type
 
 from .types import ServiceT
+
+
+class ServiceState(enum.Enum):
+    INIT = "init"
+    RUNNING = "running"
+    STOPPING = "stopping"
+    CRASHED = "crashed"
+    SHUTDOWN = "shutdown"
+
+
+class ServiceError(Exception):
+    pass
+
+
+class ServiceAlreadyRunError(ServiceError):
+    pass
+
+
+class ServiceNotRunError(ServiceError):
+    pass
 
 
 class ServiceWithCallbacks(ServiceT):
@@ -25,6 +50,22 @@ class ServiceWithCallbacks(ServiceT):
 
 
 class Service(ServiceWithCallbacks):
+    @classmethod
+    def from_awaitable(cls: Type[Service], coro: Awaitable[Any]) -> Service:
+        class _SericeFromAwaitable(cls):  # type: ignore
+            @Service.task
+            async def task(self) -> None:
+                await coro
+
+        return _SericeFromAwaitable()
+
+    def __init__(self) -> None:
+        self._state = ServiceState.INIT
+        self._restart_count = 0
+        self._should_stop = False
+        self._tasks: list[asyncio.Task] = []
+        self._stopped = asyncio.Event()
+
     def add_dependency(self, service: ServiceT) -> ServiceT:
         raise NotImplementedError(self)
 
@@ -44,7 +85,12 @@ class Service(ServiceWithCallbacks):
         raise NotImplementedError(self)
 
     async def start(self) -> None:
-        raise NotImplementedError(self)
+        if self._state is not ServiceState.INIT:
+            raise ServiceAlreadyRunError(self._state)
+        for task in self._collect_tasks():
+            async_task = asyncio.create_task(task(self))
+            self._tasks.append(async_task)
+        self._state = ServiceState.RUNNING
 
     async def maybe_start(self) -> bool:
         raise NotImplementedError(self)
@@ -53,7 +99,26 @@ class Service(ServiceWithCallbacks):
         raise NotImplementedError(self)
 
     async def stop(self) -> None:
-        raise NotImplementedError(self)
+        if self._stopped.is_set():
+            return
+        self._state = ServiceState.STOPPING
+        self._should_stop = True
+        running_tasks = [task for task in self._tasks if not task.done()]
+        if running_tasks:
+            await asyncio.wait(
+                running_tasks,
+                return_when=asyncio.ALL_COMPLETED,
+                timeout=0,
+            )
+            for task in running_tasks:
+                if not task.done():
+                    task.cancel()
+            await asyncio.wait(
+                running_tasks,
+                return_when=asyncio.ALL_COMPLETED,
+                timeout=None,
+            )
+        self._stopped.set()
 
     def service_reset(self) -> None:
         raise NotImplementedError(self)
@@ -62,7 +127,48 @@ class Service(ServiceWithCallbacks):
         raise NotImplementedError(self)
 
     async def wait_until_stopped(self) -> None:
-        raise NotImplementedError(self)
+        if self._stopped.is_set():
+            return
+        if self._state is not ServiceState.RUNNING:
+            raise ServiceNotRunError(self._state)
+        await self._stopped.wait()
 
     def set_shutdown(self) -> None:
         raise NotImplementedError(self)
+
+    @classmethod
+    def task(cls, fun: _Task) -> _Task:
+        fun._mode_task = True  # type: ignore
+        return fun
+
+    @classmethod
+    def timer(cls, interval: float) -> Callable[[_Task], _Task]:
+        def _decorate(fun: _Task) -> _Task:
+            fun._mode_timer = interval  # type: ignore
+            return fun
+
+        return _decorate
+
+    def _make_timer_task(self, fun: _Task, interval: float) -> _Task:
+        @wraps(fun)
+        async def _timer(self: Service) -> None:
+            while self._should_stop is False:
+                await asyncio.sleep(interval)
+                if not self._should_stop:
+                    await fun(self)
+
+        return _timer
+
+    def _collect_tasks(self):
+        for klass in self.__class__.__mro__:
+            if not issubclass(klass, Service):
+                continue
+            for attr_value in klass.__dict__.values():
+                if getattr(attr_value, "_mode_task", False) is True:
+                    yield attr_value
+                seconds = getattr(attr_value, "_mode_timer", None)
+                if seconds is not None:
+                    yield self._make_timer_task(attr_value, seconds)
+
+
+_Task = Callable[[Any], Awaitable[None]]
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,14 +1,21 @@
+import asyncio
 from contextlib import asynccontextmanager, contextmanager
+from time import monotonic
 from typing import AsyncGenerator, Generator
 
 import pytest
 
-from mode.service import Service
+from mode.service import Service, ServiceAlreadyRunError, ServiceNotRunError
 
 pytestmark = [pytest.mark.asyncio]
 
 
 class ServiceStub(Service):
+    def __init__(self) -> None:
+        super().__init__()
+        self.task1_run = 0
+        self.timer1_run = 0
+
     async def on_first_start(self) -> None:
         raise NotImplementedError(self)
 
@@ -27,6 +34,15 @@ class ServiceStub(Service):
     async def on_restart(self) -> None:
         raise NotImplementedError(self)
 
+    @Service.task
+    async def task1(self) -> None:
+        await asyncio.sleep(0)
+        self.task1_run += 1
+
+    @Service.timer(interval=0.01)
+    async def timer1(self) -> None:
+        self.timer1_run += 1
+
 
 @contextmanager
 def stub_contextmanager() -> Generator[None, None, None]:
@@ -36,6 +52,20 @@ def stub_contextmanager() -> Generator[None, None, None]:
 @asynccontextmanager
 async def stub_asynccontextmanager() -> AsyncGenerator[None, None]:
     yield None
+
+
+async def test_from_awaitable() -> None:
+    calls: list[int] = []
+
+    async def call() -> None:
+        calls.append(1)
+
+    service = Service.from_awaitable(call())
+
+    assert isinstance(service, Service)
+    await service.start()
+    await service.stop()
+    assert calls == [1]
 
 
 async def test_on_start() -> None:
@@ -114,7 +144,18 @@ async def test_add_async_context() -> None:
 async def test_start() -> None:
     service = ServiceStub()
 
-    with pytest.raises(NotImplementedError):
+    await service.start()
+    await asyncio.sleep(0.02)
+
+    assert service.task1_run == 1
+    assert service.timer1_run > 0
+
+
+async def test_start_on_already_started_service() -> None:
+    service = ServiceStub()
+    await service.start()
+
+    with pytest.raises(ServiceAlreadyRunError):
         await service.start()
 
 
@@ -132,11 +173,35 @@ async def test_crash() -> None:
         await service.crash(ValueError())
 
 
-async def test_stop() -> None:
+async def test_stop_on_not_started_service() -> None:
     service = ServiceStub()
 
-    with pytest.raises(NotImplementedError):
-        await service.stop()
+    await service.stop()
+
+
+async def test_stop_on_started_service() -> None:
+    service = ServiceStub()
+    await service.start()
+
+    await service.stop()
+
+
+async def test_stop_on_already_stopped_service() -> None:
+    service = ServiceStub()
+    await service.stop()
+
+    await service.stop()
+
+
+async def test_stop_on_long_running_service_cancels_tasks() -> None:
+    long_runing_service = Service.from_awaitable(asyncio.sleep(10))
+    await long_runing_service.start()
+
+    time0 = monotonic()
+    await long_runing_service.stop()
+    time1 = monotonic()
+
+    assert time1 - time0 < 1
 
 
 def test_service_reset() -> None:
@@ -153,11 +218,35 @@ async def test_restart() -> None:
         await service.restart()
 
 
-async def test_wait_until_stopped() -> None:
+async def test_wait_until_stopped_raises_for_not_started_service() -> None:
     service = ServiceStub()
 
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ServiceNotRunError):
         await service.wait_until_stopped()
+
+
+async def test_wait_until_stopped_for_stopped_service() -> None:
+    service = ServiceStub()
+    await service.stop()
+
+    await service.wait_until_stopped()
+
+
+async def test_wait_until_stopped_for_running_service() -> None:
+    async def _stop_later():
+        await asyncio.sleep(0.05)
+        await service.stop()
+
+    service = ServiceStub()
+    await service.start()
+    loop = asyncio.get_running_loop()
+    loop.create_task(_stop_later())
+
+    time0 = monotonic()
+    await service.wait_until_stopped()
+    time1 = monotonic()
+
+    assert time1 - time0 > 0.05
 
 
 def test_set_shutdown() -> None:
