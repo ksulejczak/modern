1d6ef7f7bd30aaf26920b60adae79678c3f19fa3
Krzysztof Sulejczak <krzysiek@sulejczak.eu>
2022-05-18 23:01:46 +0200

    Add simple int codec

--- modern/codecs/__init__.py
+++ modern/codecs/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["BytesCodec", "Codec", "CodecError", "StrCodec"]
+__all__ = ["BytesCodec", "Codec", "CodecError", "IntCodec", "StrCodec"]
 
 from .base import Codec, CodecError
 from .codec_bytes import BytesCodec
+from .codec_int import IntCodec
 from .codec_str import StrCodec
