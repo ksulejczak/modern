e431dd67fff665cd481b911628da9d9b4bfdd515
Krzysztof Sulejczak <krzysiek@sulejczak.eu>
2022-10-04 08:49:34 +0200

    Allow chaining codecs

--- modern/codecs/base.py
+++ modern/codecs/base.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import ClassVar, Generic, TypeVar
 
 T = TypeVar("T")
@@ -15,3 +17,29 @@ class Codec(Generic[T]):
 
     def out(self, value: T) -> bytes:
         raise NotImplementedError(self)
+
+    def __or__(self, right: Codec[T]) -> OrCodec[T]:
+        return OrCodec(self, right)
+
+
+OT = TypeVar("OT")
+
+
+class OrCodec(Codec[OT]):
+    name = "__or__"
+
+    def __init__(self, left: Codec[OT], right: Codec[OT]) -> None:
+        self._left = left
+        self._right = right
+
+    def in_(self, data: bytes) -> OT:
+        for op in (self._left, self._right):
+            try:
+                return op.in_(data)
+            except CodecError:
+                pass
+        else:
+            raise CodecError(data)
+
+    def out(self, value: OT) -> bytes:
+        return self._left.out(value)
--- tests/codecs/test_base.py
+++ tests/codecs/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from modern.codecs.base import Codec
+from modern.codecs.base import Codec, CodecError
 
 
 def test_in_raises() -> None:
@@ -15,3 +15,58 @@ def test_out_raises() -> None:
 
     with pytest.raises(NotImplementedError):
         codec.out(1)
+
+
+def test_or_codec_in_returns_left() -> None:
+    value = b"test text"
+    codec: Codec[str] = STR_CODEC | STR_FAILING_CODEC
+
+    in_value = codec.in_(value)
+
+    assert in_value == "test text"
+
+
+def test_or_codec_in_returns_right() -> None:
+    value = b"test text"
+    codec: Codec[str] = STR_FAILING_CODEC | STR_CODEC
+
+    in_value = codec.in_(value)
+
+    assert in_value == "test text"
+
+
+def test_or_codec_raises_if_both_sides_fail() -> None:
+    value = b"test text"
+    codec: Codec[str] = STR_FAILING_CODEC | STR_FAILING_CODEC
+
+    with pytest.raises(CodecError):
+        codec.in_(value)
+
+
+def test_or_codec_out_value_from_left_side() -> None:
+    value = "test text"
+    codec: Codec[str] = STR_CODEC | STR_FAILING_CODEC
+
+    out_value = codec.out(value)
+
+    assert out_value == b"test text"
+
+
+class StrTestCodec(Codec[str]):
+    def in_(self, data: bytes) -> str:
+        return data.decode()
+
+    def out(self, value: str) -> bytes:
+        return value.encode()
+
+
+class FailingStrTestCodec(Codec[str]):
+    def in_(self, data: bytes) -> str:
+        raise CodecError(data)
+
+    def out(self, value: str) -> bytes:
+        return value.encode() + b"SENTINEL"
+
+
+STR_CODEC = StrTestCodec()
+STR_FAILING_CODEC = FailingStrTestCodec()
