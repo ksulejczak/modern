cae91dcae766fda458c7a885c1c20b91112dbfc9
Krzysztof Sulejczak <krzysiek@sulejczak.eu>
2022-12-07 19:17:38 +0100

    Export Service

--- modern/__init__.py
+++ modern/__init__.py
@@ -1,3 +1,17 @@
-__all__ = ["__version__"]
+__all__ = [
+    "Service",
+    "ServiceAlreadyRunError",
+    "ServiceError",
+    "ServiceNotRunError",
+    "ServiceState",
+    "__version__",
+]
 
+from .service import (
+    Service,
+    ServiceAlreadyRunError,
+    ServiceError,
+    ServiceNotRunError,
+)
+from .types import ServiceState
 from .version import __version__
